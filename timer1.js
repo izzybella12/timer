c81074f08b4491070bc1f7b1c72c822c8be4c6e6
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-04-07 20:52:08 +0000

    used prosses.argv to set a beep timer

---
+++ timer1.js
@@ -0,0 +1,18 @@
+let time = process.argv
+time.splice(0, 2);
+let interval = 100
+
+let timerFunction = function(numbers) {
+  for (let num of numbers) {
+    let realNum = Number(num);
+    if (typeof realNum == 'number') {
+      setTimeout (() => {
+        process.stdout.write('\x07');
+      }, realNum * interval);
+    }
+  }
+};
+
+timerFunction(time);
+
+
